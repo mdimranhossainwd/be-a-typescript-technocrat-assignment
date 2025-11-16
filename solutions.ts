e27e03ef42691726f762c3abfe5ccbbc536c73f6
mdimranhossainwd <mdimranhossain7498@gmail.com>
2025-11-16 20:59:56 +0600

    Add to printBookDetails function to return book all info

--- solutions.ts
+++ solutions.ts
@@ -84,3 +84,26 @@ const users: User[] = [
 ];
 
 console.log(filterActiveUsers(users));
+
+// printBookDetails function to return book all info
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book) {
+  return `Title: ${book.title}, Author: ${book.author}, Published: ${
+    book.publishedYear
+  }, Available: ${book.isAvailable ? "Yes" : "No"}`;
+}
+
+const myBook: Book = {
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+console.log(printBookDetails(myBook));
